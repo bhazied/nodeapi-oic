0f22c16b07901551a2d04541f223d1665aa9663d
Zied Ben Hadj Amor <mr.bha.zied@gmail.com>
2019-01-31 17:14:33 +0100

    cross domaine access

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ var app = express();
 //adding a body parser for url and body response as json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 var port = process.env.PORT || 3300
 
 //init all routes
